3815458b68542f618e7605c4c19676efb89c8df7
Ansh Gujral <anshgujral1997@gmail.com>
2018-01-26 16:49:16 +0530

    login state saved

--- views/js/firebaseAuth.js
+++ views/js/firebaseAuth.js
@@ -1,31 +1,38 @@
 function checkIfLoggedIn() {
-  var user = firebase.auth().currentUser
-  if (user) {
-    document.getElementById('google-signin')
-    .setAttribute('style', 'display: inline-block; visibility: hidden')
-    document.getElementById('signout')
-      .setAttribute('style', 'display: inline-block; visibility: visible')
-  } else {
-    document.getElementById('google-signin')
-      .setAttribute('style', 'display: inline-block; visibility: visible')
-    document.getElementById('signout')
-      .setAttribute('style', 'display: none; visibility: hidden')
-  }
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // console.log(user)
+      var photoURL = user.photoURL
+      document.getElementById('google-pic')
+        .setAttribute('src', photoURL)
+      document.getElementById('google-signin')
+      .setAttribute('style', 'display: inline-block; visibility: hidden')
+      document.getElementById('signout')
+        .setAttribute('style', 'display: inline-block; visibility: visible')
+    } else {
+      document.getElementById('google-signin')
+        .setAttribute('style', 'display: inline-block; visibility: visible')
+      document.getElementById('signout')
+        .setAttribute('style', 'display: none; visibility: hidden')
+    }
+  })
 }
-checkIfLoggedIn()
+window.onload = function () {
+  checkIfLoggedIn()
+}
 
 function signOut() {
+  firebase.auth().signOut()
 
   document.getElementById('google-pic')
   .setAttribute('src', '')
-  checkIfLoggedIn()
 }
 
 function signInWithGoogle() {
   var googleAuthProvider = new firebase.auth.GoogleAuthProvider
   firebase.auth().signInWithPopup(googleAuthProvider)
     .then(function(data) {
-      console.log(data)
+      // console.log(data)
       var photoURL = data.additionalUserInfo.profile.picture
 
       var myName = 'Ansh Gujral'
@@ -37,6 +44,6 @@ function signInWithGoogle() {
         checkIfLoggedIn()
     })
     .catch(function(error) {
-      console.log(error)
+      // console.log(error)
     })
 }
