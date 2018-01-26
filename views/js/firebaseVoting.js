9b1728f160430bbcd70d7ba2bd15aca223ef07d4
Ansh Gujral <anshgujral1997@gmail.com>
2018-01-27 00:25:57 +0530

    upvote functionality added

--- views/js/firebaseVoting.js
+++ views/js/firebaseVoting.js
@@ -5,11 +5,45 @@ function addRestraunt() {
   var restrauntName = restrauntInput.value
   restrauntInput.value = ''
   // return restrauntRef.push().key
-  return restrauntRef.push(restrauntName)
+  return restrauntRef.push({
+    name: restrauntName,
+    votes: 0
+  })
     .then(function() {
       window.location.reload()
     })
     .catch(function (error) {
       console.log(error)
     })
+}
+function upVote(key) {
+  var database = firebase.database()
+  var user = firebase.auth().currentUser
+  var userId = user.uid
+  var displayName = user.displayName
+  // var restrauntVotesRef = database.ref('/restraunts/' + key + '/votes' + userId)
+  var restrauntVotesRef = database.ref('/restraunts')
+                                  .child(key)
+                                  .child('/votes')
+                                  .child(userId)
+  restrauntVotesRef.push(displayName)
+                   .then(function(){
+                      window.location.reload()
+                   })
+                   .catch(function(err){
+                     console.log(err)
+                   })
+}
+
+
+function downVote(key) {
+  var database = firebase.database()
+  var user = firebase.auth().currentUser
+  var userId = user.uid
+  var displayName = user.displayName
+  // var restrauntVotesRef = database.ref('/restraunts/' + key + '/votes' + userId)
+  var restrauntVotesRef = database.ref('/restraunts')
+                                  .child(key)
+                                  .child('/votes')
+  restrauntVotesRef.remove(userId)
 }
