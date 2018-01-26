a9bdbe96fe65ef0db35c511497eb9975dcc20348
Ansh Gujral <anshgujral1997@gmail.com>
2018-01-26 15:08:04 +0530

    sign out button added

--- views/js/firebaseAuth.js
+++ views/js/firebaseAuth.js
@@ -1,3 +1,31 @@
+function checkIfLoggedIn() {
+  if (localStorage.getItem('firebase_idToken')) {
+    document.getElementById('google-pic')
+      .setAttribute('src', localStorage.getItem('google-pic'))
+    document.getElementById('google-signin')
+    .setAttribute('style', 'display: inline-block; visibility: hidden')
+    document.getElementById('signout')
+      .setAttribute('style', 'display: inline-block; visibility: visible')
+  } else {
+    document.getElementById('google-signin')
+      .setAttribute('style', 'display: inline-block; visibility: visible')
+    document.getElementById('signout')
+      .setAttribute('style', 'display: none; visibility: hidden')
+  }
+}
+checkIfLoggedIn()
+
+function signOut() {
+  localStorage.removeItem('firebase_idToken')
+  localStorage.removeItem('google-pic')
+
+  document.getElementById('google-pic')
+  .setAttribute('style', 'visibility: hidden')
+  document.getElementById('google-pic')
+  .setAttribute('src', '')
+  checkIfLoggedIn()
+}
+
 function signInWithGoogle() {
   var googleAuthProvider = new firebase.auth.GoogleAuthProvider
   firebase.auth().signInWithPopup(googleAuthProvider)
@@ -8,29 +36,13 @@ function signInWithGoogle() {
       var myName = 'Ansh Gujral'
       var idToken = data.credential.idToken
       localStorage.setItem('firebase_idToken', idToken)
-      localStorage.setItem('google_photo', photoURL)
+      localStorage.setItem('google-pic', photoURL)
 
       document.getElementById('google-pic')
         .setAttribute('src', photoURL)
-
+        checkIfLoggedIn()
     })
     .catch(function(error) {
       console.log(error)
     })
 }
-function checkIfLoggedIn() {
-  if (localStorage.getItem('firebase_idToken')) {
-    document.getElementById('google-pic')
-      .setAttribute('src', localStorage.getItem('google_photo'))
-    document.getElementById('google-signin')
-    .setAttribute('style', 'display: inline-block; visibility: hidden')
-    document.getElementById('signout')
-      .setAttribute('style', 'display: none; visibility: visible')
-  } else {
-    document.getElementById('google-signin')
-      .setAttribute('style', 'display: inline-block; visibility: visible')
-    document.getElementById('signout')
-      .setAttribute('style', 'display: none; visibility: hidden')
-  }
-}
-checkIfLoggedIn()
